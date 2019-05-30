375249a213e4fc45ccb52d9cc62fd9e8e9ca0769
neutrino400 <neutrino400@gmail.com>
2019-05-31 00:42:10 +0900

    Made user.php. Fill in later

---
+++ user.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="whatever">
+  <meta name="author" content="whatever">
+
+  <link href="./css/styles.css" rel="stylesheet">
+
+  <title>USERNAMEPLACEHOLDER</title>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <div class="topnav">
+    <a class="active" href="index.php">Home</a>
+    <a href="auction.php">Auction</a>
+    <a href="selling.php">Sales</a>
+    <a class="rightAlign" href="#login">LOGIN</a>
+  </div>
+
+  <!-- <hr> -->
+
+<!-- #TODO
+FOR  Joseph and Sue
+Make user's personal page
+it should contain the user's selling item
+the auction that the user attended(?)
+the items that user bought
+User infor-->
+
+  <!-- FOOTER -->
+  <div class="footer">
+    <p>Copyright &copy; HaJoSue 2019</p>
+  </div>
+
+
+</body>
+
+</html>
