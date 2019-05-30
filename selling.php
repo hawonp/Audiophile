c50de9f4e9bbe7ed5907c236385275ce0c48601d
sujeongyoun2 <46368980+sujeongyoun2@users.noreply.github.com>
2019-05-30 19:31:16 +0900

    Hey yall ;-) Selling

--- selling.php
+++ selling.php
@@ -26,6 +26,35 @@
 
   <p> hello it sales </p>
 
+ <?php
+  $servername = "localhost";
+  $username = "user";
+  $password = "hey";
+  $dbname = "auction_db";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } 
+
+$sql = "SELECT * FROM Sells S, Item I Where S.iid = I.iid";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Item name</th><th>Sell Price</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["iname"]."</td><td>".$row["sellprice"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+?>
+    
   <!-- FOOTER -->
   <div class="footer">
     <p>Copyright &copy; HaJoSue 2019</p>
