f11d3e467683f44eea86f667060229c5f486b097
sujeongyoun2 <46368980+sujeongyoun2@users.noreply.github.com>
2019-06-05 02:05:46 +0900

    Character constraints catch

--- auction.php
+++ auction.php
@@ -84,30 +84,28 @@
     <h2 class="my-4">Welcome to Auction Page!</h2>
     <p class="lead"> Bid on the Current Auctions</p>
     <hr class="my-4">
-
   </div>
   </center>
 
   <div class="container-fluid selling">
-    <?php
-
-      $result = mysqli_query($conn, "SELECT * FROM Auction a, Item i WHERE a.iid = i.iid");
-      $numRow = mysqli_num_rows($result);
+  <?php
+      $result = mysqli_query($conn, "SELECT * FROM Auction a, Item i WHERE a.iid = i.iid ORDER BY a.iid");
       $inc = 0;
 
-      if ($numRow > 0) {
+      if (mysqli_num_rows($result) > 0) {
 
         echo "<table class = \"table table-hover\"><thread><tr><th>Item name</th> <th>Current bidder</th> <th>Current bid price</th><th>Auction end date</th><th>Bidding Amount Entry</th></tr></thread>";
-
         echo "<tbody>";
+
         while($row = mysqli_fetch_array($result)) {
           echo "<tr><td>".$row['iname']."</td><td>".$row['email']."</td><td>".$row['curr_bid']."</td><td>".$row['end_date']."</td>
-          <td><form name = \"increase_bid\" method = 'POST' action = auction.php>
-          <input type=text size=30 max='2147483647' name=\"bid_".$inc."\"/>
-          <input type=submit name = \"but_".$inc."\" value=\"RACE!\"/>
+          <td><form name=\"increase_bid\" method='POST' action=auction.php>
+          <input type=text size=30 name=\"bid_".$inc."\"/>
+          <input type=submit name=\"but_".$inc."\" value=\"RACE!\"/>
           </form></td></tr>";
           ++$inc;
         }
+
         echo "</tbody>";
         echo "</table>";
 
@@ -118,80 +116,99 @@
       }
 
       function increaseBid($conn, $input, $textinput){
-       
+        
         $raw_results = mysqli_query($conn, "SELECT * FROM Auction a, Item i Where a.iid = i.iid"); 
         $count = 0;
 
-        if($input == 0){
+        if(is_numeric($_POST['bid_0'])){
+            if($input == 0){
 
-          $results = mysqli_fetch_array($raw_results);
-          if((int)$textinput<= $results['curr_bid']){
+                $results = mysqli_fetch_array($raw_results);
+              
+              if((int)$textinput <= $results['curr_bid']){
           
-              $message = "The bidding value is too small!";
-              echo "<script type='text/javascript'>alert('$message');</script>";
+           //   $message = "The bidding value is too small!";
+                $message = "This is the one!"; 
+                echo "<script type='text/javascript'>alert('$message');</script>";
           
-          } else {  
-
-              $message = "Your bidding was successful.";
-              mysqli_query($conn, "UPDATE Auction SET curr_bid = ".(int)$textinput.", email = \"".$_SESSION['email']."\" WHERE iid = ".$results["iid"]);
-              echo "<script type='text/javascript'>alert('$message');</script>";
-          }
-        } else {
-
-          while($results = mysqli_fetch_array($raw_results)){
-            
-            if($count == $input){
-
-              if((int)$textinput<= $results['curr_bid']){
-
-                $message = "The bidding value is too small!";
-                echo "<script type='text/javascript'>alert('$message');</script>";
-
-              } else { 
-
-                $message = "Your bidding was successful.";
-                mysqli_query($conn, "UPDATE Auction SET curr_bid = ".(int)$textinput.", email = \"".$_SESSION['email']."\" WHERE iid = ".$results["iid"]);
-                echo "<script type='text/javascript'>alert('$message');</script>";
+              } else {  
+              
+                  $message = "Your bidding was successful.";
+			            mysqli_query($conn, "UPDATE Auction SET curr_bid = ".(int)$textinput.", email = \"".$_SESSION['email']."\" WHERE iid = ".$results["iid"]);
+                  echo "<script type='text/javascript'>alert('$message');</script>";
 
               } 
-            }
+            } else {
 
-            ++$count;
+              while($results = mysqli_fetch_array($raw_results)){
+            
+                if($count == $input){
 
+                    if((int)$textinput <= $results['curr_bid']){
+                        $message = "This is the 222222222!"; 
+                        //$message = "The bidding value is too small!";
+                        echo "<script type='text/javascript'>alert('$message');</script>";
+
+                    } else { 
+
+                      $message = "Your bidding was successful.";
+				              mysqli_query($conn, "UPDATE Auction SET curr_bid = ".(int)$textinput.", email = \"".$_SESSION['email']."\" WHERE iid = ".$results["iid"]);
+                      echo "<script type='text/javascript'>alert('$message');</script>";
+
+                    } 
+                }
+
+                ++$count;
+
+                }
+
+              }
+          
+          } else {
+             $message = "Please enter numbers only!"; 
+            echo "<script type='text/javascript'>alert('$message');</script>";
           }
+          echo ('<meta http-equiv="refresh" content="0;url='.$_SERVER['HTTP_REFERER'].'">');
+        } // The end of function
 
-        }
-
-        header("Refresh:0");
-        
-      }
-
+     
       if(isset($_POST['but_0'])){
         increaseBid($conn, 0, $_POST['bid_0']);
       } else if(isset($_POST['but_1'])){
         increaseBid($conn, 1, $_POST['bid_1']);
-      } else if(isset($_POST['but_2'])){
+		  } else if(isset($_POST['but_2'])){
         increaseBid($conn, 2, $_POST['bid_2']);
-      }else if(isset($_POST['but_3'])){
+      } else if(isset($_POST['but_3'])){
+    
         increaseBid($conn, 3, $_POST['bid_3']);
+	    	
       }else if(isset($_POST['but_4'])){
+     
         increaseBid($conn, 4, $_POST['bid_4']);
+	    	
       }else if(isset($_POST['but_5'])){
+        
         increaseBid($conn, 5, $_POST['bid_5']);
+        
       }else if(isset($_POST['but_6'])){
+       
         increaseBid($conn, 6, $_POST['bid_6']);
+        
       }else if(isset($_POST['but_7'])){
+        
         increaseBid($conn, 7, $_POST['bid_7']);
+        
       }else if(isset($_POST['but_8'])){
+        
         increaseBid($conn, 8, $_POST['bid_8']);
+        
       }else if(isset($_POST['but_9'])){
+        
         increaseBid($conn, 9, $_POST['bid_9']);
-      }
-
-      
-    ?>
-
-  </div>
+        
+	  }
+?>
+</div>
 
   <div class="py-5 footer bg-dark">
     <div class="container">
